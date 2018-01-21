9a11d71e57d872e8b41ce64c719b04db5907cc27
wepe <weipforever@gmail.com>
2018-01-21 15:30:49 +0800

    Loss,Metric

--- tgboost-java/src/main/ClassList.java
+++ tgboost-java/src/main/ClassList.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ClassList {
@@ -21,4 +22,32 @@ public class ClassList {
     public void initialize_pred(double first_round_pred){
         Arrays.fill(pred, first_round_pred);
     }
+
+    public void update_pred(double eta){
+        for(int i=0;i<dataset_size;i++){
+//            pred[i] += eta * corresponding_tree_node[i].leaf_score;
+        }
+    }
+
+    public void update_grad_hess(Loss loss,double scale_pos_weight){
+        grad = loss.grad(pred,label);
+        hess = loss.hess(pred,label);
+        if(scale_pos_weight != 1.0){
+            for(int i=0;i<dataset_size;i++){
+                if(label[i]==1){
+                    grad[i] *= scale_pos_weight;
+                    hess[i] *= scale_pos_weight;
+                }
+            }
+        }
+    }
+
+    public void sampling(ArrayList<Byte> row_mask){
+        for(int i=0;i<dataset_size;i++){
+            grad[i] *= row_mask.get(i);
+            hess[i] *= row_mask.get(i);
+        }
+    }
+
+
 }
--- tgboost-java/src/main/Loss.java
+++ tgboost-java/src/main/Loss.java
@@ -1,4 +1,73 @@
 package main;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.Arrays;
+
 public class Loss {
+    public double[] grad(double[] pred,double[] label){
+        throw new NotImplementedException();
+    }
+
+    public double[] hess(double[] pred,double[] label){
+        throw new NotImplementedException();
+    }
+}
+
+
+class SquareLoss extends Loss{
+
+    public double[] grad(double[] pred,double[] label){
+        double[] ret = new double[pred.length];
+        for(int i=0;i<ret.length;i++){
+            ret[i] = pred[i] - label[i];
+        }
+        return ret;
+    }
+
+    public double[] hess(double[] pred,double[] label){
+        double[] ret = new double[pred.length];
+        Arrays.fill(ret,1.0);
+        return ret;
+    }
+
+}
+
+
+class LogisticLoss extends Loss{
+    private double clip(double val){
+        if(val<0.00001){
+            return 0.00001;
+        }else if(val>0.99999){
+            return 0.99999;
+        }else {
+            return val;
+        }
+    }
+
+    private double[] transform(double[] pred){
+        double[] ret = new double[pred.length];
+        for(int i=0;i<ret.length;i++){
+            ret[i] = clip(1.0 / (1.0+Math.exp(-pred[i])));
+        }
+        return ret;
+    }
+
+    public double[] grad(double[] pred,double[] label){
+        pred = transform(pred);
+        double[] ret = new double[pred.length];
+        for(int i=0;i<ret.length;i++){
+            ret[i] = (1-label[i])/(1-pred[i]) - label[i]/pred[i];
+        }
+        return ret;
+    }
+
+    public double[] hess(double[] pred,double[] label){
+        pred = transform(pred);
+        double[] ret = new double[pred.length];
+        for(int i=0;i<ret.length;i++){
+            ret[i] = label[i]/Math.pow(pred[i],2.0) + (1-label[i])/Math.pow(1-pred[i],2.0);
+        }
+        return ret;
+    }
 }
